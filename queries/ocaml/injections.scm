5eb7442d3ed6ed4a68aaa77243e488d2bd100077
TJ DeVries <devries.timothyj@gmail.com>
2023-05-29 00:52:08 -0400

    do js: for antonioooo

--- queries/ocaml/injections.scm
+++ queries/ocaml/injections.scm
@@ -27,3 +27,26 @@
  (#contains? @_attribute_id "graphql" "relay")
  (#set! injection.language "graphql")
  (#set! injection.include-children true))
+
+; javascript injection
+((extension
+   (attribute_id) @_attribute_id
+   (attribute_payload
+    (expression_item
+     (quoted_string
+      (quoted_string_content) @injection.content))))
+
+ (#contains? @_attribute_id "raw" "bs.raw" "melange.raw")
+ (#set! injection.language "javascript")
+ (#set! injection.include-children true))
+
+((item_extension
+   (attribute_id) @_attribute_id
+   (attribute_payload
+    (expression_item
+     (quoted_string
+      (quoted_string_content) @injection.content))))
+
+ (#contains? @_attribute_id "raw" "bs.raw" "melange.raw")
+ (#set! injection.language "javascript")
+ (#set! injection.include-children true))
