53df6f15b7a2a7017031006f9d4a833e2e50c884
TJ DeVries <devries.timothyj@gmail.com>
2023-08-30 16:16:48 -0500

    feat: add menhir ft

--- plugin/ocaml.lua
+++ plugin/ocaml.lua
@@ -2,8 +2,10 @@
 vim.filetype.add {
   extension = {
     mli = "ocaml.interface",
+    mly = "ocaml.menhir",
   },
 }
 
 -- If you have `ocaml_interface` parser installed, it will use it for `ocaml.interface` files
 vim.treesitter.language.register("ocaml_interface", "ocaml.interface")
+vim.treesitter.language.register("menhir", "ocaml.menhir")
