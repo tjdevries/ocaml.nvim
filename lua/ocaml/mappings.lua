4d3cce81a56c015214fed9ef006698d6da286e00
TJ DeVries <devries.timothyj@gmail.com>
2024-06-04 20:56:55 -0400

    feat: add destruct mapping function

--- lua/ocaml/mappings.lua
+++ lua/ocaml/mappings.lua
@@ -1,8 +1,9 @@
 local M = {}
 
+--- Promote the current file via `dune promote`
 M.dune_promote_file = function()
   if vim.bo.modified then
-    print "Save before trying to promote"
+    vim.notify "Save before trying to promote"
     return
   end
 
@@ -28,6 +29,28 @@ M.dune_promote_file = function()
       ["start"] = { 0, 0 },
       ["end"] = { -1, -1 },
     },
+  }
+end
+
+--- Destruct a record or variant under the cursor
+---@param opts { enumerate_cases: boolean | nil }?: Options for the destruct action
+M.destruct = function(opts)
+  opts = opts or {}
+  opts.enumerate_cases = vim.F.if_nil(opts.enumerate_cases, false)
+
+  -- Keep the old destruct kind, in case of older LSP.
+  --    Newer version has multiple destruct kinds we can use.
+  local only = { "destruct" }
+  if opts.enumerate_cases then
+    table.insert(only, "destruct (enumerate cases)")
+  else
+    table.insert(only, "destruct-line (enumerate cases, use existing match)")
+  end
+
+  vim.lsp.buf.code_action {
+    apply = true,
+    ---@diagnostic disable-next-line: missing-fields
+    context = { only = only },
   }
 end
 
