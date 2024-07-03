8735069ce4267940e1038c7a2942881cf15481ce
TJ DeVries <devries.timothyj@gmail.com>
2024-07-03 14:51:14 -0400

    queries: add my sql query injections

--- queries/ocaml/injections.scm
+++ queries/ocaml/injections.scm
@@ -50,3 +50,25 @@
  (#contains? @_attribute_id "raw" "bs.raw" "melange.raw")
  (#set! injection.language "javascript")
  (#set! injection.include-children true))
+
+
+  ;(value_definition ; [82, 0] - [83, 60]
+  ;  (attribute_id) ; [82, 4] - [82, 9]
+  ;  (let_binding ; [82, 10] - [83, 60]
+  ;    pattern: (package_pattern ; [82, 10] - [82, 30]
+  ;      (module_name)) ; [82, 18] - [82, 29]
+  ;    body: (string ; [83, 2] - [83, 60]
+  ;      (string_content)))) ; [83, 3] - [83, 59]
+((value_definition
+  (attribute_id) @_attribute_id
+  (let_binding
+    body: (string (string_content) @injection.content)))
+ (#contains? @_attribute_id "query")
+ (#set! injection.language "sql"))
+
+((value_definition
+  (attribute_id) @_attribute_id
+  (let_binding
+    body: (quoted_string (quoted_string_content) @injection.content)))
+ (#contains? @_attribute_id "query")
+ (#set! injection.language "sql"))
